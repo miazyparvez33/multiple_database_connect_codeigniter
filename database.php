8486e90783a4abbcfdc27d9de99b3506d9ca8fff
Parvez Ahmed <miazyparvez33@users.noreply.github.com>
2016-11-30 13:26:05 +0600

    Create database.php

---
+++ database.php
@@ -0,0 +1,33 @@
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'test1';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+ 
+//Another database connection.
+ 
+$db['db2']['hostname'] = 'localhost';
+$db['db2']['username'] = 'root';
+$db['db2']['password'] = '';
+$db['db2']['database'] = 'test2';
+$db['db2']['dbdriver'] = 'mysql';
+$db['db2']['dbprefix'] = '';
+$db['db2']['pconnect'] = FALSE;
+$db['db2']['db_debug'] = TRUE;
+$db['db2']['cache_on'] = FALSE;
+$db['db2']['cachedir'] = '';
+$db['db2']['char_set'] = 'utf8';
+$db['db2']['dbcollat'] = 'utf8_general_ci';
+$db['db2']['swap_pre'] = '';
+$db['db2']['autoinit'] = TRUE;
+$db['db2']['stricton'] = FALSE;
